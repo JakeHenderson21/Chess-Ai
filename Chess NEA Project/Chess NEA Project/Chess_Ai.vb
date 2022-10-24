1371c42c5cfa4e84b3bb2d3cec86973b30eae2eb
JakeHenderson21 <jake.henderson.2005@outlook.com>
2022-10-24 16:44:46 +0100

    Chess ai

--- Chess NEA Project/Chess NEA Project/Chess_Ai.vb
+++ Chess NEA Project/Chess NEA Project/Chess_Ai.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Chess_Ai
+    Public Sub CheckPawns()
 
+    End Sub
+    Public Sub CheckRooks()
+
+    End Sub
+    Public Sub CheckBishops()
+
+    End Sub
+    Public Sub CheckKnights()
+
+    End Sub
+    Public Sub CheckQueen()
+
+    End Sub
+    Public Sub CheckKing()
+
+    End Sub
 End Class
--- Chess NEA Project/Chess NEA Project/MainMenu.vb
+++ Chess NEA Project/Chess NEA Project/MainMenu.vb
@@ -1,6 +1,4 @@
 ﻿Public Class MainMenu
-
-
     Private Sub ExitBtn_Click(sender As Object, e As EventArgs) Handles ExitBtn.Click
         End
     End Sub
