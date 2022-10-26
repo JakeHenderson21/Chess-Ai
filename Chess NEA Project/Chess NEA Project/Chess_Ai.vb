2ce61981d4fff5e7e9b634199a5ad176833c28ce
JakeHenderson21 <jake.henderson.2005@outlook.com>
2022-10-26 15:28:22 +0100

    Start on neural network plus mini max

--- Chess NEA Project/Chess NEA Project/Chess_Ai.vb
+++ Chess NEA Project/Chess NEA Project/Chess_Ai.vb
@@ -1,6 +1,24 @@
 ﻿Public Class Chess_Ai
-    Public Sub CheckPawns()
+    Private NumberlegalMoves As Integer
+    Private inputLayers(NumberlegalMoves) As Double
+    Private hiddenLayer(3, NumberlegalMoves) As Double
+    Private outputLayer(NumberlegalMoves + 1) As Double
+    Private hiddenLayerWeights(3, NumberlegalMoves + 1) As Double
+    Private hiddenBias(3, NumberlegalMoves) As Double
+    Private outputBias(NumberlegalMoves) As Double
+    Private FirstCheckNumber As Integer
+    Public Sub New()
 
+    End Sub
+    Public Sub CheckPawns()
+        Dim chesscolour As ChessPiece.Chesscolour
+        For Each piece In Check_Checkmate.Bpawn
+            Dim Pawns As New Pawn(piece.Left, piece.Top, chesscolour, piece, ChessBoard.FirstCheck(FirstCheckNumber))
+            FirstCheckNumber += 1
+            Pawns.SetColour()
+            Pawns.CheckMoves()
+            ChessBoard.chess_piece = piece
+        Next
     End Sub
     Public Sub CheckRooks()
 
