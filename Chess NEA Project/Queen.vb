33fd7ad7cec1742045136f3cf2ceb855b10e8aa1
Jake Henderson <116122032+JakeHenderson21@users.noreply.github.com>
2023-01-25 09:06:10 +0000

    Re-uploading project

---
+++ Chess NEA Project/Queen.vb
@@ -0,0 +1,12 @@
+﻿Public Class Queen
+    Inherits ChessPiece
+    Public Sub New(ByVal X As Integer, ByVal Y As Integer, ByVal chess_colour As Chesscolour, ByVal piece As Button)
+        MyBase.New(X, Y, chess_colour, piece)
+    End Sub
+    'Sets the boundaries of the loops for checking for possible moves
+    Public Sub SetLoopBoundaries()
+        startofloop = 0
+        endofloop = 7
+        count = 0
+    End Sub
+End Class
