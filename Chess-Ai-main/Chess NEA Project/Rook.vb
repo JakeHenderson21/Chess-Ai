9c02dd36de0d90952a7f72973ab7c1ae623cca86
Jake Henderson <116122032+JakeHenderson21@users.noreply.github.com>
2023-03-03 09:51:08 +0000

    Further Imrpovements

---
+++ Chess-Ai-main/Chess NEA Project/Rook.vb
@@ -0,0 +1,12 @@
+﻿Public Class Rook
+    Inherits ChessPiece
+    Public Sub New(ByVal X As Integer, ByVal Y As Integer, ByVal chess_colour As Chesscolour, ByVal piece As Button)
+        MyBase.New(X, Y, chess_colour, piece)
+    End Sub
+    'Sets the boundaries of the loops for checking for possible moves
+    Public Sub SetLoopBoundaries()
+        startofloop = 0
+        endofloop = 3
+        count = 0
+    End Sub
+End Class
